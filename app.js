46a4adaa7b7f07db3f8c698750ae2ef5fe451c26
poipoinn <85345914+poipoinn@users.noreply.github.com>
2021-06-30 18:06:57 +0900

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+const a = 5;
+const b = 2;
+let myName = "soyounglee";
+
+console.log(a+b);
+console.log(a*b);
+console.log(a/b);
+console.log("hello " + myName);
+
+myName = "soyoung";
+
+console.log("your name is " + myName);
