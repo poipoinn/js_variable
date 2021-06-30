c2f590d22f71dde49c03017365ec77bb36d019c9
poipoinn <2676sy@gmail.com>
2021-06-30 17:45:37 +0900

    js_variables

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const a = 5;
 const b = 2;
-let myName = "soyoung";
+let myName = "soyounglee";
 
 console.log(a+b);
 console.log(a*b);
 console.log(a/b);
 console.log("hello " + myName);
 
-myName = "soyounglee";
+myName = "soyoung";
 
 console.log("your name is " + myName);
